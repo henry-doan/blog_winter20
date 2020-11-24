4e903a17d6e37aad769a7bf0e97b22a64d643c0f
Henry Doan <henrydoan96@gmail.com>
2020-11-23 20:57:15 -0700

    finish lecture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,7 +23,21 @@ class PagesController < ApplicationController
   end
 
   def edit
-    @page = Page.find(params[:index])
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit 
+    end
+  end
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
   end
 
   private

6b0e59325b8cde3b9a566303b8902e8680cc0d6f
Henry Doan <henrydoan96@gmail.com>
2020-11-23 20:34:10 -0700

    added controller and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,33 @@
 class PagesController < ApplicationController
+  # GET all page 
   def index
+    @pages = Page.all
+    # render index.html.erb
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
+  end
+
+  def create 
+    @page = Page.new(pages_params)
+    if @page.save 
+      redirect_to pages_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @page = Page.find(params[:index])
   end
+
+  private
+    def pages_params
+      params.require(:page).permit(:title, :author, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :pages
 end
